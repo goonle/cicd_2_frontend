6701b12cc6f659cb0e198399026cc2a6a52719db
goonle <junhuh741@gmail.com>
2025-04-21 16:15:19 +1200

    [Feature] Update Blog

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,29 +1,106 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './BlogCard.module.css';
-// import styles from './BlogDetail.module.css';
+import { AUTH_PUT, AUTH_GET_ONE } from '../utils/api-helper';
+import { useToast } from '../context/ToastContext';
+import Loader from './Loader';
+
 const BlogCard = (props) => {
   const { blog } = props;
-  
+  const { handleToast } = useToast();
+  const [blogData, setBlogData] = useState({
+    id: blog.id,
+    title: "loading...",
+    content: "loading...",
+  });
+  const [mode, setMode] = useState("view");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const successFunc = (res) => {
+      const data = res.data;
+      setBlogData(data || { title: "loading...", content: "loading..." });
+    }
+    const errorFunc = (err) => {
+      handleToast("Error fetching blog", "error");
+    }
+
+    AUTH_GET_ONE("blogs/", blog.id, successFunc, errorFunc)
+
+  }, [blog.id]); // fetch only when blog.id changes
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const title = formData.get('title');
+    const content = formData.get('content');
+    const blog = { title, content };
+
+    const successFunc = (res) => {
+      props.callbackFunc(res.data);
+      handleToast("Blog updated successfully", "success");
+      setMode("view");
+      setBlogData(res.data);
+      props.clickClose();
+    }
+    const errorFunc = (err) => {
+      handleToast("Error updating blog", "error");
+      console.error('Error updating blog:', err);
+    }
+
+    setLoading(true);
+    AUTH_PUT(`blogs/${blogData.id}/`, blog, successFunc, errorFunc);
+  }
+
   return (
-    <div className={styles.modal}>
-      <div className={styles.card}>
-        <div className={`${styles.header}`}>
-          <div className={styles.flexContainer}>
-            <h2>{blog.title}</h2>
-            <button className={styles.closeButton} onClick={props.clickClose}>X</button>
+    <>
+    {<Loader show={loading} />}
+      <div className={styles.modal}>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.card}>
+            <div className={`${styles.header}`}>
+              <div className={styles.flexContainer}>
+                {mode === "view" ?
+                  <h2>{blogData.title}</h2> :
+                  <input
+                    name="title"
+                    type="text"
+                    value={blogData.title}
+                    onChange={(e) => setBlogData({ ...blogData, title: e.target.value })}
+                  />}
+                <div className={styles.buttonGroup}>
+                  {mode === "view" ?
+                    <>
+                      <button onClick={() => { setMode("edit") }}>Edit</button>
+                      <button >Delete</button>
+                    </> :
+                    <button type="submit">Save</button>
+                  }
+                  <button className={styles.closeButton} onClick={props.clickClose}>X</button>
+                </div>
+              </div>
+              <p className={styles.meta}>
+                by <strong>{blogData.author}</strong> | {new Date(blogData.created_at).toLocaleDateString()}
+              </p>
+            </div>
+            <div className={styles.content}>
+              {mode === "view" ?
+                <p>{blogData.content}</p> :
+                <textarea
+                  name="content"
+                  value={blogData.content}
+                  onChange={(e) => setBlogData({ ...blogData, content: e.target.value })}
+                />
+              }
+
+            </div>
+            <div className={styles.footer}>
+              ❤️ {blogData.likes_count} {blogData.likes_count === 1 ? 'like' : 'likes'}
+            </div>
           </div>
-          <p className={styles.meta}>
-            by <strong>{blog.author}</strong> | {new Date(blog.created_at).toLocaleDateString()}
-          </p>
-        </div>
-        <div className={styles.content}>
-          <p>{blog.content}</p>
-        </div>
-        <div className={styles.footer}>
-          ❤️ {blog.likes_count} {blog.likes_count === 1 ? 'like' : 'likes'}
-        </div>
+
+        </form>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/utils/api-helper.js
+++ src/utils/api-helper.js
@@ -79,3 +79,89 @@ export const AUTH_POST = (url, data, suc=noAction, fail=noAction, fin=noAction) 
         }
     });
 }
+
+export const AUTH_PUT = (url, data, suc=noAction, fail=noAction, fin=noAction) => {
+    if (!localStorage.getItem('token')) {
+        return {
+            data: {},
+            status: 401,
+            message: "Unauthorized"
+        }
+    }
+    return axios.put(`${BASE_URL}${url}`, data, {
+        headers: {
+            Authorization: `Token ${localStorage.getItem('token')}`
+        },
+    }).then(res => {
+        if (res.status === 200  && !!suc) {
+            suc(res);
+        }
+
+        return {
+            data: res.data,
+            status: res.status,
+            message: res.statusText
+        };
+    }).catch(err => {
+        if (!!fail) {
+            fail(err);
+        }
+
+        return {
+            data: {},
+            status: err.response?.status || 500,
+            message: err.response?.statusText || "Server Error"
+        };
+    }).finally(() => {
+        if (!!fin) {
+            fin();
+        }
+    });
+}
+
+export const AUTH_GET_ONE = (url, id, suc=noAction, fail=noAction, fin=noAction) => {
+    if (!localStorage.getItem('token')) {
+        return {
+            data: {},
+            status: 401,
+            message: "Unauthorized"
+        }
+    }
+    if (!id) {
+        return {
+            data: {},
+            status: 400,
+            message: "Bad Request"
+        }
+    }
+    
+    return axios.get(`${BASE_URL}${url}${id}/`, {
+        headers: {
+            Authorization: `Token ${localStorage.getItem('token')}`
+        },
+    }).then(res => {
+        if (res.status === 200  && !!suc) {
+            suc(res);
+        }
+
+        return {
+            data: res.data,
+            status: res.status,
+            message: res.statusText
+        };
+    }).catch(err => {
+        if (!!fail) {
+            fail(err);
+        }
+
+        return {
+            data: {},
+            status: err.response?.status || 500,
+            message: err.response?.statusText || "Server Error"
+        };
+    }).finally(() => {
+        if (!!fin) {
+            fin();
+        }
+    });
+}
